ea0cdd4521be9229e6687a2a0aaaa2d38183562a
valencra <valencra@mcmaster.ca>
2017-12-03 12:56:09 -0500

    Added SessionFactory creation in Application class

--- src/main/java/com/teamtreehouse/contactmgr/Application.java
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -1,6 +1,20 @@
 package com.teamtreehouse.contactmgr;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class Application {
+    // Hold a reusable reference to a SessionFactory (since we only need one)
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        // Create a StandardServiceRegistry
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
     public static void main(String[] args) {
 
     }
