a97bed07d2a58c116a5234cc1d92cab9914498c3
valencra <valencra@mcmaster.ca>
2017-12-03 20:08:57 -0500

    Added methods for getting contacts by ID, updating contacts, and deleting contacts

--- src/main/java/com/teamtreehouse/contactmgr/Application.java
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -22,17 +22,73 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        // Save contact
         Contact contact = new ContactBuilder("Chris", "Ramacciotti")
                 .withEmail("rama@teamtreehouse.com")
                 .withPhone(7735556666L)
                 .build();
+        int id = save(contact);
 
-        // Save contact
-        save(contact);
-
-        // Display a list of contacts
+        // Display a list of contacts before the update
+        System.out.printf("%n%nBefore update%n%n");
         fetchAllContacts().stream().forEach(System.out::println);
 
+        // Get the persisted contact
+        Contact c = findContactById(id);
+
+        // Update the contact
+        c.setFirstName("Beatrix");
+
+        // Persist the changes
+        System.out.printf("%n%nUpdating...%n%n");
+        update(c);
+        System.out.printf("%n%nUpdate complete!%n%n");
+
+        // Display a list of contacts after the update
+        System.out.printf("%n%nAfter update%n%n");
+        fetchAllContacts().stream().forEach(System.out::println);
+
+        // Delete the contact
+        System.out.printf("%n%nDeleting...%n%n");
+        delete(c);
+        System.out.printf("%n%nDeletion complete!%n%n");
+
+        // Display a list of contacts after the deletion
+        System.out.printf("%n%nAfter deletion%n%n");
+        fetchAllContacts().stream().forEach(System.out::println);
+    }
+
+    private static int save(Contact contact) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the session to save the contact
+        int id = (int) session.save(contact);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
+
+        return id;
+    }
+
+    private static Contact findContactById(int id) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Retrieve the persistent object (or null if not found)
+        Contact contact = session.get(Contact.class, id);
+
+        // Close the session
+        session.close();
+
+        // Return the object
+        return contact;
     }
 
     @SuppressWarnings("unchecked")
@@ -52,15 +108,15 @@ public class Application {
         return contacts;
     }
 
-    private static void save(Contact contact) {
+    private static void update(Contact contact) {
         // Open a session
         Session session = sessionFactory.openSession();
 
         // Begin a transaction
         session.beginTransaction();
 
-        // Use the session to save the contact
-        session.save(contact);
+        // Use the session to update the contact
+        session.update(contact);
 
         // Commit the transaction
         session.getTransaction().commit();
@@ -69,5 +125,20 @@ public class Application {
         session.close();
     }
 
+    private static void delete(Contact contact) {
+        // Open a session
+        Session session = sessionFactory.openSession();
 
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the session to update the contact
+        session.delete(contact);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
+    }
 }
